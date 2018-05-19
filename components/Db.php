5cb024873743c012f0ab22087bb20f124abc426f
Denis <den4@me.com>
2018-05-19 14:45:49 +0300

    start lesson 5

--- components/Db.php
+++ components/Db.php
@@ -2,16 +2,17 @@
 
 class Db
 {
-
+    
     public static function getConnection()
     {
         $paramsPath = ROOT . '/config/db_params.php';
-        $params = include $paramsPath;
+        $params = include($paramsPath);
+        
 
         $dsn = "mysql:host={$params['host']};dbname={$params['dbname']}";
-        $db = new PDO($dsn, $params['user'], $params['password']); //add for debug sql - , [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-        // $db->exec("set names utf8");
-
+        $db = new PDO($dsn, $params['user'], $params['password']);
+        $db->exec("set names utf8");
+        
         return $db;
     }
 
--- models/Product.php
+++ models/Product.php
@@ -3,7 +3,7 @@
 class Product
 {
 
-    const SHOW_BY_DEFAULT = 3;
+    const SHOW_BY_DEFAULT = 6;
 
     /**
      * Returns an array of products
